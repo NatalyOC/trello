a5f01a4f9b193aaf7232836bdfbc404234bb859f
NatalyOC <noteroc@hotmail.com>
2017-11-20 05:01:39 -0500

    creando estructura y estilo v0.1

--- js/app.js
+++ js/app.js
@@ -1,7 +1,9 @@
 window.addEventListener('load', function() {
-  var addList = document.getElementById('text-list');
+  var addList = document.getElementById('text-addList');
+  var list= document.getElementById('add-list');
   addList.addEventListener('click', function() {
-    
+    addList.classList.add('hidden');
+    list.classList.add('show');
   });
 
 });
