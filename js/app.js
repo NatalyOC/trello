57d273d49f19b1e8d8b7efeb7d0fc370182f892a
NatalyOC <noteroc@hotmail.com>
2017-11-22 01:37:27 -0500

    implementando v0.0.1

--- js/app.js
+++ js/app.js
@@ -1,9 +1,12 @@
 window.addEventListener('load', function() {
   var addList = document.getElementById('text-addList');
-  var list= document.getElementById('add-list');
+  var form = document.getElementById('form');
+  var list = document.getElementById('add-list');
+
   addList.addEventListener('click', function() {
-    addList.classList.add('hidden');
-    list.classList.add('show');
+    addList.classList.toggle('display-none');
+    form.classList.toggle('display-none');
+    
   });
 
 });
