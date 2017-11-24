7a91737b4a5954cda584aad638cab3d00825eca9
NatalyOC <noteroc@hotmail.com>
2017-11-24 02:03:58 -0500

    implementando v0.0.2

--- js/app.js
+++ js/app.js
@@ -4,12 +4,12 @@ window.addEventListener('load', function() {
   var containerList = document.getElementById('add-list');
   var button = document.getElementById('list-add-button');
   var nameList = document.getElementById('list-name');
-
-  addList.addEventListener('click', function() {
-    addList.classList.toggle('display-none');
+  /* Versión 0.0.1 */
+  addList.addEventListener('click', function(event) {
+    event.target.classList.toggle('display-none');
     form.classList.toggle('display-none');
   });
-  
+  /* Versión 0.0.2 */
   button.addEventListener('click', function(event) {
     event.preventDefault();
     if (nameList.value) {
@@ -21,12 +21,62 @@ window.addEventListener('load', function() {
 
       form.parentElement.insertBefore(div, form);
     }
-    var linkList = document.createElement('a');
-    linkList.setAttribute('href', '#');
-    linkList.classList.add('open-card');
+    var linkTask = document.createElement('a');
+    linkTask.setAttribute('href', '#');
+    linkTask.classList.add('task');
     var linkTitle = document.createTextNode('Añadir una tarea...');
-    linkList.appendChild(linkTitle);
-    div.parentElement.appendChild(linkList);
+    linkTask.appendChild(linkTitle);
+    div.parentElement.appendChild(linkTask);
+    /* Versión 0.0.6 (Extra) */
+    var containerRight = document.createElement('div');
+    containerRight.classList.add('container-form-list');
+    form.parentElement.parentElement.appendChild(containerRight);
+    containerRight.appendChild(addList);
+    addList.classList.toggle('display-none');
+
+    containerRight.insertBefore(form, containerRight.firstElementChild);
+    /* Versión 0.0.3 */
+    linkTask.addEventListener('click', function(event) {
+      event.preventDefault();
+      linkTask.classList.toggle('task');
+      linkTask.classList.add('display-none');
+
+      var subForm = document.createElement('form');
+      var textArea = document.createElement('textarea');
+      textArea.classList.add('list-name-input');
+
+      var btnTask = document.createElement('input');
+      btnTask.setAttribute('value', 'Añadir');
+      btnTask.setAttribute('type','submit');
+      btnTask.classList.add('list-add-button');
+      subForm.appendChild(textArea);
+      subForm.appendChild(btnTask);
+      event.target.parentElement.appendChild(subForm); 
+      btnTask.disabled = false;
+      textArea.focus();
+      // textArea.addEventListener('keyup')
+      
+
+      /* Versión 0.0.4 - Versión 0.0.5 */
+      btnTask.addEventListener('click', function(event) {
+        event.preventDefault();
+        var titleTask = document.createTextNode(btnTask.previousElementSibling.value.trim());
+        var divTask = document.createElement('div');
+        divTask.classList.add('task-title');
+        divTask.appendChild(titleTask);
+        var fatherButton = event.target.parentElement;
+        fatherButton.classList.toggle('display-none');
+        fatherButton.parentElement.appendChild(divTask);
+        divTask.parentElement.appendChild(fatherButton.previousElementSibling);
+        fatherButton.parentElement.lastElementChild.classList.toggle('task');
+        fatherButton.parentElement.lastElementChild.classList.toggle('display-none');
+
+      });
+    });
+
+
   });
+
+
 });
 
